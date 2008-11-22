f826c5f86f43ca7b30d87d98d90a0ab55eb08ffe
nikhil.handigol <nikhil.handigol@17d5c184-ae2b-11dd-9be4-9b4a7b2c739f>
2008-11-22 08:10:33 +0000

    Modifying client prog to speak to plumb

--- trunk/src/plumb.c
+++ trunk/src/plumb.c
@@ -10,6 +10,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define PORT 20000
+
 void error(char *msg)
 {
     perror(msg);
@@ -21,16 +23,12 @@ int main(int argc, char *argv[])
      int sockfd, portno, clilen;
      struct sockaddr_in serv_addr, cli_addr;
      pid_t eth0_pid = -1, eth1_pid = -1;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) {
         error("ERROR opening socket");
      }
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+     portno = PORT;
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
