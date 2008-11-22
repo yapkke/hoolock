57e0160ba5b36e8e0b464e79c307997867860b74
nikhil.handigol <nikhil.handigol@17d5c184-ae2b-11dd-9be4-9b4a7b2c739f>
2008-11-22 07:16:17 +0000

    plumber program complete

--- trunk/src/plumb.c
+++ trunk/src/plumb.c
@@ -1,129 +1,106 @@
-/*
-** plumb.c -- a stream socket server 
-** makes and breaks vde pipes
-*/
+/* A simple server using TCP
+ * Makes and breaks vde pipes
+   The port number is passed as an argument */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
 
-#define PORT "2020"  // the port users will be connecting to
-
-#define BACKLOG 10	 // how many pending connections queue will hold
-
-void sigchld_handler(int s)
+void error(char *msg)
 {
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+    perror(msg);
+    exit(1);
 }
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+int main(int argc, char *argv[])
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+     int sockfd, portno, clilen;
+     struct sockaddr_in serv_addr, cli_addr;
+     pid_t eth0_pid = -1, eth1_pid = -1;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) {
+        error("ERROR opening socket");
+     }
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     printf("Listening on port number %d....\n", portno);
+
+     while (1) {
+	     char buffer[256];
+	     int newsockfd, n;
+
+	     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	     if (newsockfd < 0) 
+		     error("ERROR on accept");
+
+	     bzero(buffer,256);
+
+	     // Read the command
+	     n = read(newsockfd, buffer, 255);
+	     if (n < 0) 
+		     error("ERROR reading from socket");
+
+	     // Based on the command fork a process
+	     if(strncmp(buffer, "make", 4) == 0) {
+		     if(buffer[5] == '0') {
+			     if((eth0_pid = fork()) == 0) {
+				     /* child process */
+				     close(newsockfd);
+				     close(sockfd);
+				     printf("Make: mobile0SW <====> openflow0SW\n");
+				     system("dpipe vde_plug /home/nikhilh/vm/Hoolock/vde/ctlmobile0SW = wirefilter = vde_plug /home/nikhilh/vm/Hoolock/vde/ctlopenflow0SW");
+			     }
+		     }
+		     else if(buffer[5] == '1') {
+			     if((eth1_pid = fork()) == 0) {
+				     /* child process */
+				     close(newsockfd);
+				     close(sockfd);
+				     printf("Make: mobile1SW <====> openflow1SW\n");
+				     system("dpipe vde_plug /home/nikhilh/vm/Hoolock/vde/ctlmobile1SW = wirefilter = vde_plug /home/nikhilh/vm/Hoolock/vde/ctlopenflow1SW");
+			     }
+		     }
+		     else {
+			     printf("Bad make command! Shouldn't be seeing this.\n");
+		     }
+	     }
+	     else if(strncmp(buffer, "break", 4) == 0) {
+		     if(buffer[5] == '0') {
+			     printf("Break: mobile0SW <====> openflow0SW\n");
+			     kill(eth0_pid, 9);
+			     eth0_pid = -1;
+		     }
+		     else if(buffer[5] == '1') {
+			     printf("Break: mobile1SW <====> openflow1SW\n");
+			     kill(eth1_pid, 9);
+			     eth1_pid = -1;
+		     }
+		     else {
+			     printf("Bad break command! Shouldn't be seeing this.\n");
+		     }
+	     }
+
+	     n = write(newsockfd,"Plumber executed the command!", 29);
+	     if (n < 0) 
+		     error("ERROR writing to socket");
+	     close(newsockfd);
+     }
+     return 0; 
 }
-
-int main(void)
-{
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) == -1) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		return 2;
-	}
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	printf("server: waiting for connections...\n");
-
-	while(1) {  // main accept() loop
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
-
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("server: got connection from %s\n", s);
-
-		if (!fork()) { // this is the child process
-			close(sockfd); // child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
-			close(new_fd);
-			exit(0);
-		}
-		close(new_fd);  // parent doesn't need this
-	}
-
-	return 0;
-}
-
