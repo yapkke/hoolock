a8e9e342e222e0dd6ea7aba49cd9abb33d034867
mchan.cs.stanford <mchan.cs.stanford@17d5c184-ae2b-11dd-9be4-9b4a7b2c739f>
2009-03-08 11:19:57 +0000

    Completed test-hoolock.c, need compilation.

--- src/test-hoolock.c
+++ src/test-hoolock.c
@@ -21,15 +21,46 @@ typedef __uint16_t u16;		/* ditto */
 typedef __uint8_t u8;		/* ditto */
 #include <linux/ethtool.h>
 
-//#define SIOCBONDHOOLOCKTEST 0x899a
-
 #define DEFAULT_BOND_NAME "bond0"
 #define DEFAULT_SLEEP_TIME 5000 // fed to usleep()
+
+#define NUM_INTERFACES 2
+#define NUM_ESSIDS 2
+
+char interfaces[NUM_INTERFACES][64] = {"ath0", "wlan0"};
+char essids[NUM_ESSIDS][64] = {"cleanslatewifi1", "cleanslatewifi2"};
+
+/* ----------------------------------------------------------------------------- */
+
+void set_active_slave_command(char* command, char* bond_name, char* interface){
+	sprintf(command, "ifenslave -c %s %s", bond_name, interface);
+}
+
+void set_association_command(char* command, char* interface, char* essid){
+	sprintf(command, "iwconfig %s essid %s ap auto", interface, essid);
+}
+
+void set_association_command(char* command, char* interface, char* essid){
+	sprintf(command, "iwconfig %s essid \"xxxx\" ap off", interface);
+}
+
+void execute_command(char* command){
+	fprintf(stderr, "Executing: %s\n", command);	
+	system(command);
+}
+
+void send_ioctl_command(int skfd, int ioctl_command, struct ifreq *ifr){
+	if (ioctl(skfd, ioctl_command, ifr) < 0) {
+		printf("ioctl call failed :(\n");
+		exit(-1);
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	char bond_name[64] = DEFAULT_BOND_NAME; // Interface name
 	int sleep_time = DEFAULT_SLEEP_TIME;  // Sleep duration in usec
+	char command[128];
 
 	if(argc > 1) {
 		strcpy(bond_name, argv[1]);
@@ -46,11 +77,65 @@ int main(int argc, char *argv[])
 		printf("Failed to open socket\n");
 		return -1;
 	}
-	
 	struct ifreq ifr;
 	strcpy(ifr.ifr_name, bond_name);
 
-	
+	// Variables for switching
+	int cur_interface, cur_essid;
+	int next_interface, next_essid;
+
+	/* Initial bonding tx interface */
+	cur_interface = 0;
+	set_active_slave_comand(command, bond_name, cur_interface);
+	execute_command(command);
+
+	/* Initial AP association */
+	cur_essid = 0;
+	set_association_command(command, cur_interface, cur_essid);
+	execute_command(command);
+
+	/* Switching */
+	while(1){
+		char menu_selection[16];
+		printf("=== Hoolock client program ===\n");
+		printf("1) Switch AP\n");
+		printf("q) Quit\n");
+
+		printf("\nEnter option:\n");
+		scanf("%s", menu_selection);
+
+		if(menu_selection[0] == 'q') break;
+		switch(menu_selection[0]){
+			case '1':
+				printf("Starting switching, sleep time = %d usec", sleep_time);
+				next_interface = (cur_interface + 1) % NUM_INTERFACES;
+				next_essid = (cur_essid + 1) % NUM_ESSIDS;
+				
+				// Associate with next essid
+				set_association_command(command, next_interface, next_essid);
+				execute_command(command);
+
+				// Send "make" ioctl command to bonding driver
+				send_ioctl_command(skfd, SIOCBONDHOOLOCKMAKE, &ifr);
+
+				// Sleep
+				usleep(sleep_time);
+
+				// Send "break" ioctl command to bonding driver
+				send_ioctl_command(skfd, SIOCBONDHOOLOCKBREAK, &ifr);	
+
+				// Deassociate with original essid
+				set_deassociation_command(command, cur_interface);
+				execute_command(command);
+				
+				cur_interface = next_interface;
+				cur_essid = next_essid;
+				break;
+			default:
+				fprintf(stderr, "Error! Invalid option.\n");
+		}
+	}
+
 	if (ioctl(skfd, SIOCBONDHOOLOCKTEST, &ifr) < 0) {
 		printf("ioctl call failed :(\n");
 		return -1;
